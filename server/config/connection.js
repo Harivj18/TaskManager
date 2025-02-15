cecab219398343b57098ac99e8ebae86f9a85ec2
Harivj18 <hariharanamir1798@gmail.com>
2025-02-15 17:21:33 +0530

    Commit for vercel deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,7 @@ module.exports = {
         try {
             await mongoose.connect(process.env.MONGO_URI);
             console.log('connection.js : connectMongo => Mongo DB Connection Established Successfully !!');
+            return
         } catch (error) {
             console.log('connection.js: connectMongo => Error while making mongo connection from mongodb',error);
             throw error;
