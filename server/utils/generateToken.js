7193a5b914599a2ce37605a976fc69b0f56d2f25
Harivj18 <hariharanamir1798@gmail.com>
2025-02-15 17:44:07 +0530

    Commit for vercel deploy

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const setJWTCookie = async (userName, res) => {
         res.cookie('sessionToken', token, {
             maxAge: 15 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "Strict",
+            sameSite: "None",
             secure:true
         })
 
